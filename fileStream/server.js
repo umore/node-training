6f1157894403600e53f922d7ef9c78ff54687b53
umore <umore@equinix.com>
2015-02-27 14:33:47 -0800

    Supports File uploads

--- fileStream/server.js
+++ fileStream/server.js
@@ -1,20 +1,64 @@
 var http = require('http'),
     fileStream = require('./fileStream.js'),
     fileSystem = require('fs');
-    url = require("url");
+    url = require('url');
+    path = require('path'),
+    formidable = require('formidable'),
+    util = require('util');
 
 http.createServer(function (request, response) {
 
-    var pathname = url.parse(request.url).pathname;
-    var filePath = pathname.slice(1, pathname.length);
+    if (request.url == '/upload' && request.method.toLowerCase() == 'post') {
+        // parse a file upload
+        var form = new formidable.IncomingForm();
 
-    var stat = fileSystem.statSync(filePath);
-    response.writeHead(200, {
-        'Content-Type': 'text/plain',
-        'Content-Length': stat.size
-    });
+        form.on('fileBegin', function(name, file) {
+            file.path = path.resolve(__dirname, 'files', file.name);
+        });
 
-    var fileReadStream = fileStream.getFileReadStream(filePath);
-    fileReadStream.pipe(response);
+        form.parse(request, function(err, fields, files) {
+          response.writeHead(200, {'content-type': 'text/plain'});
+          response.write('received upload:\n\n');
+          response.end(util.inspect({fields: fields, files: files}));
+        });
 
-}).listen(8080);
+        return;
+    }
+
+    if ((request.url.indexOf('/download') === 0) && (request.method.toLowerCase() == 'get')) {
+        
+        var pathname = url.parse(request.url).pathname;
+        pathname = pathname.slice('/download'.length, pathname.length);
+        var filePath = pathname.slice(1, pathname.length);
+        filePath = path.resolve(__dirname, 'files', filePath);
+        fileSystem.exists(filePath, function (exists) {
+            if (exists) {
+                var stat = fileSystem.statSync(filePath);
+                response.writeHead(200, {
+                    'Content-Type': 'text/plain',
+                    'Content-Length': stat.size
+                });
+
+                var fileReadStream = fileStream.getFileReadStream(filePath);
+                fileReadStream.pipe(response);    
+            }else{
+                response.writeHead(404);
+                response.write(filePath + " not found");
+                response.end();
+            }
+        });
+
+        return;
+    }
+    
+    // show a file upload form
+    response.writeHead(200, {'content-type': 'text/html'});
+    response.end(
+        '<form action="/upload" enctype="multipart/form-data" method="post">'+
+        '<input type="text" name="title"><br>'+
+        '<input type="file" name="upload" multiple="multiple"><br>'+
+        '<input type="submit" value="Upload">'+
+        '</form>'
+    );
+
+}).listen(8081);
